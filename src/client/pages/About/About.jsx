3e2c1d5bd48c17cbe5459819212595b82bcd0783
BoroDinS <sergey.borodin.lg@gmail.com>
2023-05-16 22:51:17 +0200

    add some text for better fit

--- src/client/pages/About/About.jsx
+++ src/client/pages/About/About.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import './AboutPageStyle.css'
+import React from "react";
+import "./AboutPageStyle.css";
 
 export const About = () => {
   return (
-    <section className='section about-section'>
-      <h1 className='section-title'>about us</h1>
-      <p className='section-description'>
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p className="section-description">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Error, non
         illum quod ut soluta est quisquam laborum dicta vero aliquid deleniti
         eveniet quo explicabo debitis dolorum odio saepe delectus ducimus!
@@ -22,8 +22,25 @@ export const About = () => {
         Dolor voluptatibus minima animi doloremque ab? Explicabo sequi
         necessitatibus vel ab cum ratione voluptatibus rerum a repudiandae
         tempore exercitationem quisquam sed ut suscipit in, ducimus eos. At
-        impedit eius earum!
+        impedit eius earum Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Error, non illum quod ut soluta est quisquam laborum dicta vero
+        aliquid deleniti eveniet quo explicabo debitis dolorum odio saepe
+        delectus ducimus! Ducimus eaque nostrum temporibus in obcaecati vel?
+        Magnam inventore qui labore beatae ullam illum voluptatum repudiandae
+        totam saepe, magni nesciunt dolorem ea suscipit neque vitae eaque
+        impedit, voluptate autem deleniti. Beatae ipsam tempore culpa
+        repudiandae deleniti minus blanditiis magnam porro aliquam totam quis,
+        cum aspernatur quod molestiae maiores expedita recusandae, voluptatibus
+        aut sunt, veritatis nulla. Pariatur praesentium corporis iste
+        temporibus. Enim laborum cumque, nostrum autem officia at dolores est
+        quam beatae perspiciatis, labore dolor? Sapiente rerum natus recusandae
+        nihil laboriosam expedita. Quidem at reprehenderit ut voluptates error,
+        cumque impedit quisquam. Dolor voluptatibus minima animi doloremque ab?
+        Explicabo sequi necessitatibus vel ab cum ratione voluptatibus rerum a
+        repudiandae tempore exercitationem quisquam sed ut suscipit in, ducimus
+        eos. At impedit eius earum ipsum dolor sit amet consectetur adipisicing
+        elit. Error, non illum quod ut soluta est quisquam laborum dicta vero!
       </p>
     </section>
-  )
-}
+  );
+};
